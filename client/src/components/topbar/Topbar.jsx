37e680e5cd3408875fc5673a5298d2a731ce2d33
ranja <seolin9340@gmail.com>
2022-02-08 04:31:55 +0900

    Feat(client): Styling UI

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,49 +1,42 @@
-import React from "react";
 import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
-import { Person, Search, Chat, Notifications } from "@material-ui/icons";
-
-const Topbar = () => {
-	return (
-		<div className='topbarContainer'>
-			<div className='topbarLeft'>
-				<span className='logo'>RanjaSocial</span>
-			</div>
-
-			<div className='topbarCenter'>
-				<div className='searchbar'>
-					<Search className='searchIcon' />
-					<input
-						type='text'
-						placeholder='친구, 게시글, 비디오를 찾아보세요'
-						className='searchInput'
-					/>
-				</div>
-			</div>
-
-			<div className='topbarRight'>
-				<div className='topbarLinks'>
-					<div className='topbarLink'>Homepage</div>
-					<div className='topbarLink'>Timeline</div>
-				</div>
-				<div className='topbarIcons'>
-					<div className='topbarIconItem'>
-						<Person />
-						<span className='topbarIconBadge'>1</span>
-					</div>
-					<div className='topbarIconItem'>
-						<Chat />
-						<span className='topbarIconBadge'>1</span>
-					</div>
-					<div className='topbarIconItem'>
-						<Notifications />
-						<span className='topbarIconBadge'>1</span>
-					</div>
-				</div>
-				<img src='/assets/person/1.jpeg' alt='' className='topbarImg' />
-			</div>
-		</div>
-	);
-};
-
-export default Topbar;
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
-import React from "react";
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css"
 
-const Home = () => {
-	return (
-		<>
-			<Topbar />
-		</>
-	);
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed/>
+        <Rightbar/>
+      </div>
+    </>
+  );
+}
