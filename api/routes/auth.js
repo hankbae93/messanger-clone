1216ccf325266f53343bde21082ead7f9a82e728
ranja <seolin9340@gmail.com>
2022-02-07 18:33:04 +0900

    Add: Auth Route 에러 처리 및 프론트 전송 추가

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -20,7 +20,7 @@ router.post("/register", async (req, res) => {
 		const user = await newUser.save();
 		res.status(200).json(user);
 	} catch (error) {
-		console.error(error);
+		res.status(500).json(error);
 	}
 });
 
@@ -38,7 +38,7 @@ router.post("/login", async (req, res) => {
 
 		res.status(200).json(user);
 	} catch (error) {
-		console.error(error);
+		res.status(500).json(error);
 	}
 });
 
