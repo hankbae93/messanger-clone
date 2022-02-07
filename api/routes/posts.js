771c345a8a6759862fa9d52e23e018d74ecc0f08
ranja <seolin9340@gmail.com>
2022-02-08 00:47:51 +0900

    Feat(api): 전체 타임라인 가져오기 API 추가
    - 사용자 작성글과 팔로잉 유저의 작성글 포함 전체 데이터 조회

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 const router = require("express").Router();
 
@@ -46,4 +47,51 @@ router.delete("/:id", async (req, res) => {
 		res.status(500).json(error);
 	}
 });
+
+// like a post
+router.put("/:id/like", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post.likes.includes(req.body.userId)) {
+			await post.updateOne({ $push: { likes: req.body.userId } });
+			res.status(200).json("포스트 좋아요 성공");
+		} else {
+			await post.updateOne({ $pull: { likes: req.body.userId } });
+			res.status(200).json("포스트 좋아요 취소");
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// 게시글 상세 조회
+router.get("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// 전체 게시글 타임라인 조회
+router.get("/timeline/all", async (req, res) => {
+	try {
+		// 현재 유저 정보 가져오기
+		const currentUser = await User.findById(req.body.userId);
+		// 현재 유저의 작성글 찾기
+		const userPosts = await Post.find({ userId: currentUser._id });
+		// 현재 유저의 팔로우 리스트 멤버의 작성글 가져오기
+		const friendPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+
+		res.status(200).json(userPosts.concat(...friendPosts));
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -64,7 +64,7 @@ router.put("/:id/follow", async (req, res) => {
 
 			if (!user.followers.includes(req.body.userId)) {
 				await user.updateOne({ $push: { followers: req.body.userId } });
-				await currentUser.updateOne({ $push: { followings: req.body.userId } });
+				await currentUser.updateOne({ $push: { followings: req.params.id } });
 				res.status(200).json("팔로우되셨습니다.");
 			} else {
 				res.status(403).json("이미 팔로우한 유저입니다.");
