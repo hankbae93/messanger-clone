543454e032d069f09c59e786b8e0c121dfb08124
ranja <seolin9340@gmail.com>
2022-02-09 00:09:38 +0900

    Feat: 회원가입 API 연동 및 페이징 처리 추가

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,12 +1,12 @@
 import { useContext, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-
-import CircularProgress from "@material-ui/core/CircularProgress";
-
 import "./login.css";
 
 export default function Login() {
+	const history = useHistory();
 	const email = useRef();
 	const password = useRef();
 	const { user, isFetching, error, dispatch } = useContext(AuthContext);
@@ -18,7 +18,6 @@ export default function Login() {
 			dispatch
 		);
 	};
-	console.log(user);
 
 	return (
 		<div className='login'>
@@ -46,7 +45,7 @@ export default function Login() {
 							minLength={6}
 							required
 						/>
-						<button className='loginButton'>
+						<button className='loginButton' type='submit'>
 							{isFetching ? (
 								<CircularProgress color='white' size='20px' />
 							) : (
@@ -54,7 +53,11 @@ export default function Login() {
 							)}
 						</button>
 						<span className='loginForgot'>Forgot Password?</span>
-						<button className='loginRegisterButton' disabled={isFetching}>
+						<button
+							className='loginRegisterButton'
+							disabled={isFetching}
+							onClick={() => history.push("/register")}
+						>
 							{isFetching ? (
 								<CircularProgress color='white' size='20px' />
 							) : (
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,28 +1,84 @@
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import "./register.css";
 
 export default function Register() {
-  return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
-          <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
-          </span>
-        </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Password Again" className="loginInput" />
-            <button className="loginButton">Sign Up</button>
-            <button className="loginRegisterButton">
-              Log into Account
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const history = useHistory();
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const passwordAgain = useRef();
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		if (passwordAgain.current.value !== password.current.value) {
+			passwordAgain.current.setCustomValidity("비밀번호가 일치하지 않습니다.");
+		} else {
+			const user = {
+				username: username.current.value,
+				email: email.current.value,
+				password: password.current.value,
+			};
+			try {
+				const res = await axios.post("/auth/register", user);
+				history.push("/login");
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	};
+
+	return (
+		<div className='login'>
+			<div className='loginWrapper'>
+				<div className='loginLeft'>
+					<h3 className='loginLogo'>RanjaSocial</h3>
+					<span className='loginDesc'>
+						Connect with friends and the world around you on RanjaSocial.
+					</span>
+				</div>
+				<form className='loginRight' onSubmit={handleClick}>
+					<div className='loginBox'>
+						<input
+							placeholder='Username'
+							className='loginInput'
+							ref={username}
+							required
+						/>
+						<input
+							type='email'
+							placeholder='Email'
+							className='loginInput'
+							ref={email}
+							required
+						/>
+						<input
+							type='password'
+							placeholder='Password'
+							className='loginInput'
+							ref={password}
+							required
+							minLength={6}
+						/>
+						<input
+							placeholder='Password Again'
+							className='loginInput'
+							ref={passwordAgain}
+							required
+						/>
+						<button type='submit' className='loginButton'>
+							Sign Up
+						</button>
+						<button
+							className='loginRegisterButton'
+							onClick={() => history.push("/login")}
+						>
+							Log into Account
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
