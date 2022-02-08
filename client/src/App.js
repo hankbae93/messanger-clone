f2adba80121ff33af015dfa9e84aec95e18f0f4f
ranja <seolin9340@gmail.com>
2022-02-08 20:23:51 +0900

    Feat(client): Router 세팅 및 이미지 url 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
 function App() {
-	return <Profile />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/login'>
+					<Login />
+				</Route>
+				<Route exact path='/register'>
+					<Register />
+				</Route>
+				<Route exact path='/profile/:name'>
+					<Profile />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function Post({ post }) {
 	const [like, setLike] = useState(post.like);
 	const [isLiked, setIsLiked] = useState(false);
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
 	const likeHandler = () => {
 		setLike(isLiked ? like - 1 : like + 1);
@@ -18,7 +19,10 @@ export default function Post({ post }) {
 					<div className='postTopLeft'>
 						<img
 							className='postProfileImg'
-							src={Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+							src={
+								PF +
+								Users.filter((u) => u.id === post?.userId)[0].profilePicture
+							}
 							alt=''
 						/>
 						<span className='postUsername'>
@@ -32,19 +36,19 @@ export default function Post({ post }) {
 				</div>
 				<div className='postCenter'>
 					<span className='postText'>{post?.desc}</span>
-					<img className='postImg' src={post.photo} alt='' />
+					<img className='postImg' src={PF + post.photo} alt='' />
 				</div>
 				<div className='postBottom'>
 					<div className='postBottomLeft'>
 						<img
 							className='likeIcon'
-							src='assets/like.png'
+							src={`${PF}like.png`}
 							onClick={likeHandler}
 							alt=''
 						/>
 						<img
 							className='likeIcon'
-							src='assets/heart.png'
+							src={`${PF}heart.png`}
 							onClick={likeHandler}
 							alt=''
 						/>
